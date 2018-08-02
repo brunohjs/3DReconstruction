61e9dff13f60463b0297bdf59d83db22af793280
Bruno Justo <brunohjs@gmail.com>
2018-08-02 18:29:42 -0300

    Adição da permissão de execução de todos os outputs.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import glob
+
 from modules.aux.files import saveFile, log
 
 from modules.pre_processing import *
@@ -9,23 +11,23 @@ from modules.comparison import *
 
 
 
-def main():
+def main(args):
     t0 = time.time()
 
     'Pré-processamento'
-    dataset = getDataset(sys.argv[1])
+    dataset = getDataset(args)
     dataset = splitDataset(dataset)
     dataset = ordenizeDataset(dataset)
     dataset = getHigherBin(dataset)
     pcloud_original = generatePointCloud(dataset)
     
-    saveFile(pcloud_original, sufix='original')
+    saveFile(pcloud_original, args, sufix='original')
 
     'Filtragem'
     pcloud = removeOutliers(pcloud_original)
-    saveFile(pcloud, sufix='filt1')
+    saveFile(pcloud, args, sufix='filt1')
     pcloud = removeNoise(pcloud)
-    saveFile(pcloud, sufix='filt2')
+    saveFile(pcloud, args, sufix='filt2')
     pcloud = smoothing(pcloud)
     
     'Reconstrução'
@@ -35,8 +37,8 @@ def main():
     pcloud_result = comparison(pcloud, pcloud_original)
 
     'Salvar em arquivo'
-    saveFile(pcloud)
-    saveFile(pcloud_result, sufix='result')
+    saveFile(pcloud, args)
+    saveFile(pcloud_result, args, sufix='result')
 
     t1 = time.time()
     dt = str(round(t1 - t0, 2))+'s'
@@ -44,4 +46,12 @@ def main():
     
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        files = glob.glob('inputs/*.txt')
+        for file_ in files:
+            main(file_)
+            log('Arquivo '+file_+' lido com sucesso.')
+            #except:
+                #log('Erro ao ler o arquivo '+file_)

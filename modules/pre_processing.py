964c50cdcef2f3f5e9cb2475de9b50de5d7fa63c
Bruno Justo <brunohjs@gmail.com>
2018-08-02 18:28:29 -0300

    Adição do parâmetro 'angle' nos pontos da nuvem.

--- modules/pre_processing.py
+++ modules/pre_processing.py
@@ -91,9 +91,10 @@ def generatePointCloud(dataset):
         for beam in image:
             x, y = polar2Cartesian(beam['higher']['dist'], beam['angle'])
             refined.append({
-                'x'     :   x,
-                'y'     :   y,
-                'z'     :   beam['z'],
-                'value' :   beam['higher']['value']
+                'x':   x,
+                'y':   y,
+                'z':   beam['z'],
+                'value':   beam['higher']['value'],
+                'angle':   beam['angle']
             })
     return refined

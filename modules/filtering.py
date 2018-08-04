e389f243e8c8238595b67f0dd9af2f048f5e0b5c
Bruno Justo <brunohjs@gmail.com>
2018-08-04 16:21:15 -0300

    Modularização da função de remoção de ruídos que utiliza a PCL.

--- modules/filtering.py
+++ modules/filtering.py
@@ -1,8 +1,10 @@
 import pcl
-from modules.aux.parser import parseToArray
+from math import degrees
+
+from modules.aux.parser import parseToPointCloud
 from modules.aux.files import log
 from modules.aux.calc import distance
-from math import degrees
+
 
 
 'Filtro que diminui o ângulo horizontal da coleta de dados do sensor'
@@ -22,7 +24,7 @@ def removeMinDistPoints(point_cloud, min_dist=5):
 
     new_point_cloud = list()
     for point in point_cloud:
-        if distance((point['x'], point['y']), (0, 0)) > min_dist:
+        if distance((point['x'], point['y'], point['z']), (0, 0, 0)) > min_dist:
             new_point_cloud.append(point)
     return new_point_cloud
 
@@ -60,27 +62,34 @@ def removeOutliers(point_cloud, min_dist=5, min_val=5, angle=120):
 
 
 'Remoção de ruído da nuvem de pontos'
-def removeNoise(point_cloud):
+def staticalOutlierFilter(point_cloud):
     log("Removendo ruídos da nuvem de pontos")
 
-    'Filtro de remoção de segmentos desnecessários'
-    pc = pcl.PointCloud(parseToArray(point_cloud))
-    seg = pc.make_segmenter_normals(searchRadius=10)
-    seg.set_optimize_coefficients(True)
-    seg.set_model_type(pcl.SAC_RANSAC)
-    seg.set_method_type(2)
-    seg.set_normal_distance_weight(2)
-    seg.set_distance_threshold(1)
-    indices, model = seg.segment()
-    pc = pc.extract(indices)
+    pc = parseToPointCloud(point_cloud)
 
-    'Filtro de remoção de ruídos'
     pc = pc.make_statistical_outlier_filter()
-    pc.set_mean_k(100)
-    pc.set_std_dev_mul_thresh(1.5)
+    pc.set_mean_k(500)
+    pc.set_std_dev_mul_thresh(2)
     pc = pc.filter()
     pc = pc.to_array()
-    
+
+    return pc
+
+
+'Filtro de remoção de segmentos desnecessários'
+def segmentationFilter(point_cloud):
+    log("Segmentação da nuvem de pontos")
+
+    pc = parseToPointCloud(point_cloud)
+    seg = pc.make_segmenter()
+    seg.set_optimize_coefficients(True)
+    seg.set_model_type(pcl.SAC_RANSAC)
+    seg.set_method_type(1)
+    seg.set_distance_threshold(10)
+    indices, model = seg.segment()
+    pc = pc.extract(indices)
+    pc = pc.to_array()
+
     return pc
 
 
@@ -88,9 +97,9 @@ def removeNoise(point_cloud):
 def smoothing(point_cloud):
     log("Suavizando da nuvem de pontos")
 
-    pc = pcl.PointCloud(point_cloud)
+    pc = parseToPointCloud(point_cloud)
     pc = pc.make_moving_least_squares()
-    pc.set_search_radius(5)
+    pc.set_search_radius(6)
     pc.set_polynomial_fit(True)
     pc = pc.process()
     pc = pc.to_array()

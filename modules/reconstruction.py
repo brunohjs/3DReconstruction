ed8270b63f2a1214ece6e5c2031ef149e9232232
Bruno Justo <brunohjs@gmail.com>
2018-08-26 21:38:54 -0300

    Remoção de funções que não estão sendo mais utilizadas.

--- modules/reconstruction.py
+++ modules/reconstruction.py
@@ -5,60 +5,10 @@ from scipy.spatial import Delaunay
 from shapely.geometry import Polygon
 from modules.aux.io import log
 from modules.aux.geometry import totalArea, distance, cloud2D, projectOnPlane
-from modules.aux.parser import parserToList
 
 
-'Função que remove triângulos que possuem um lado maior que -max_distance-'
-def distanceSideTriangle(mesh, pcloud, max_distance=10):
-    log("Triangularizando os pontos")
-
-    new_mesh = list()
-    if not max_distance:
-        max_distance = float('inf')
-
-    for simplice in mesh:
-        simplice = list(it.combinations(simplice,3))
-        
-        for shape in simplice:
-            discart = False
-            for i in range(len(shape)-1):
-                for j in range(i+1, len(shape)):
-                    d = distance(pcloud[shape[i]], pcloud[shape[j]])
-                    if d > max_distance:
-                        discart = True
-                        break
-                if discart:
-                    break
-            shape = tuple([3]+sorted(list(shape)))
-            if not discart and shape not in new_mesh:
-                new_mesh.append(shape)
-            else:
-                print('ok')
-    return new_mesh
-
-
-'Filtro que remove triângulos sobrepostos'
-def overlapFilter(faces, vertex):
-    log("Removendo triângulos sobrepostos")
-
-    new_faces = list()
-    index_to_discart = list()
-
-    for i in range(len(faces)-1):
-        polygon_i = Polygon([vertex[faces[i][1]], vertex[faces[i][2]], vertex[faces[i][3]]]).buffer(-0.02)
-        discart = False
-        for j in range(i+1, len(faces)):
-            polygon_j = Polygon([vertex[faces[j][1]], vertex[faces[j][2]], vertex[faces[j][3]]])
-            if polygon_i.overlaps(polygon_j):
-                discart = True
-                break
-        if not discart:
-            new_faces.append(faces[i])
-    return new_faces
-
-
+'Função que gera o volume a partir de uma nuvem de pontos'
 def volume(pcloud):
-    pcloud = parserToList(pcloud)
     pcloud_2d = cloud2D(pcloud)
     triangulation = Delaunay(pcloud_2d)
     length = len(pcloud)
@@ -93,11 +43,9 @@ def reconstructSurface(pcloud):
 def reconstructVolume(pcloud):
     log("Reconstruindo a superfície")
 
-    pcloud = parserToList(pcloud)
     pcloud_plane = projectOnPlane(pcloud)
     vertex = pcloud_plane + pcloud
     face = volume(pcloud)
-
 
     #log(" - Área total da superfície: "+str(totalArea(vertex, face)))
 
